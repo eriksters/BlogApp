7482a5768916c67be66b7813bd8f342a60dbcc5c
Eriks Petersons <eriksters@gmail.com>
2021-09-09 22:03:16 +0200

    Blog post list item

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import BlogPostListItem from "./Components/BlogPostListItem"
-import { useFonts, Inter_400Regular } from "@expo-google-fonts/inter"
-import AppLoading from "expo-app-loading" 
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import BlogPostListItem from "./Components/BlogPostListItem";
+import {
+  useFonts,
+  Baskervville_400Regular,
+} from "@expo-google-fonts/baskervville";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
-    Inter_400Regular
+    Baskervville_400Regular,
   });
 
   if (!fontsLoaded) {
-    return <AppLoading />
+    return <AppLoading />;
   } else {
     return (
       <View style={styles.container}>
-        <BlogPostListItem />
-        <Text style={{}}>This is some text</Text>
-        <StatusBar style="auto" />
+        <BlogPostListItem
+          BlogPost={{
+            image: require("./assets/images/sadMan.jpg"),
+            description:
+              "It was a sad moment, when a local college student at UCN realized that college has not been what he thought it would be.",
+            title:
+              "Breaking news: College not as fun as you thought it would be!",
+          }}
+        />
+        <StatusBar style='auto' />
       </View>
     );
   }
@@ -28,8 +38,8 @@ const styles = StyleSheet.create({
     paddingTop: 100,
     paddingHorizontal: 10,
     flex: 1,
-    backgroundColor: '#DDD',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    backgroundColor: "#F0F0F0",
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
 });
--- Components/BlogPostListItem.js
+++ Components/BlogPostListItem.js
@@ -1,55 +1,115 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
-import {LinearGradient} from 'expo-linear-gradient';
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableHighlightComponent,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { useState } from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { Entypo } from "@expo/vector-icons";
+import Collapsible from "react-native-collapsible";
 
-const BlogPostListItem = () => {
+const BlogPostListItem = ({ BlogPost }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const Frontface = () => {
+    return <View style={{ height: 100, backgroundColor: "red" }}></View>;
+  };
+  const Backface = () => {
+    return <View style={{ height: 100, backgroundColor: "green" }}></View>;
+  };
+  const Base = () => {
+    return <View style={{ height: 100, backgroundColor: "blue" }}></View>;
+  };
+
   return (
-  <View style={styles.container}>
-    <View style={styles.imageContainer}>
-    <Image source={require('../assets/images/0266554465.jpeg')} style={styles.image}/>
-      <LinearGradient colors={['#FFF0', '#FFF']} start={{x:0, y: 0.6}} style={styles.linearGradient}>
-      </LinearGradient>
+    <View style={styles.container}>
+      <TouchableOpacity activeOpacity={0.8}>
+        <View>
+          <View style={styles.imageContainer}>
+            <Image source={BlogPost.image} style={styles.image} />
+            <LinearGradient
+              colors={["#FFF0", "#FFF"]}
+              start={{ x: 0, y: 0.6 }}
+              style={styles.linearGradient}
+            ></LinearGradient>
+          </View>
+
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>{BlogPost.title}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+
+      <Collapsible
+        collapsed={!expanded}
+        easing='quad'
+        style={styles.collapsible}
+      >
+        <Text style={styles.description}>{BlogPost.description}</Text>
+      </Collapsible>
+
+      <TouchableOpacity
+        style={styles.expandTouchArea}
+        onPress={() => {
+          setExpanded(!expanded);
+        }}
+      >
+        {expanded ? (
+          <Entypo name='chevron-thin-up' size={24} color='#555' />
+        ) : (
+          <Entypo name='chevron-thin-down' size={24} color='#555' />
+        )}
+      </TouchableOpacity>
     </View>
-    <View style={styles.titleContainer}>
-      <Text style={styles.title}>
-        Breaking news: College not as fun as you thought it would be!
-      </Text>
-    </View>
-  </View>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     width: "100%",
-    height: 200,
-    borderColor: "red",
-    borderWidth: 1,
     backgroundColor: "#fff",
     borderRadius: 10,
-    overflow: 'hidden'
+    overflow: "hidden",
   },
   imageContainer: {
-    flex: 1,
+    height: 130,
   },
   image: {
     width: "100%",
     height: "100%",
   },
-  titleContainer: {
-    height: 100
-  },
-  title: {
-    fontFamily: 'KaiseiTokumin-Regular'
-  },
-
-
   linearGradient: {
     position: "absolute",
     top: 0,
     bottom: 0,
     left: 0,
-    right: 0
+    right: 0,
+  },
+  titleContainer: {
+    paddingHorizontal: 5,
+  },
+  title: {
+    fontFamily: "Baskervville_400Regular",
+    fontSize: 22,
+  },
+  description: {
+    marginTop: 5,
+    paddingTop: 5,
+    marginHorizontal: 5,
+    borderTopColor: "#AAA",
+    borderTopWidth: 1,
+    fontSize: 18,
+    fontFamily: "Baskervville_400Regular",
+  },
+  expandTouchArea: {
+    paddingVertical: 5,
+    alignItems: "center",
   },
 });
 
