a798463fa4a214354490dd45472a52db951ef305
Eriks Petersons <eriksters@gmail.com>
2021-09-10 16:59:02 +0200

    Second Example post in API

--- Server/src/index.js
+++ Server/src/index.js
@@ -9,14 +9,23 @@ app.get("/", (req, res) => {
 });
 
 app.get("/blogposts", (req, res) => {
-  const post = {
-    title: "Post from the net",
-    description: "Welcome to the internet. Come and take a seat.",
-    image:
-      "https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg",
-  };
+  const BlogPosts = [
+    {
+      title: "Post from the net",
+      description: "Welcome to the internet. Come and take a seat.",
+      image:
+        "https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg",
+    },
+    {
+      title:
+        "Testing whether massive amounts of caffeine increase productivity?",
+      description: "I see time!",
+      image:
+        "https://wompampsupport.azureedge.net/fetchimage?siteId=7716&url=https%3A%2F%2Fwww.cancer.org%2Fcontent%2Fdam%2Fcancer-org%2Fimages%2Fphotographs%2Fsingle-use%2Fespresso-coffee-cup-with-beans-on-table-restricted.jpg%2Fjcr%3Acontent%2Frenditions%2Fcq5dam.web.1280.1280.jpeg",
+    },
+  ];
 
-  res.send({ blogPost: post });
+  res.send({ BlogPosts });
 });
 
 app.listen(port);
