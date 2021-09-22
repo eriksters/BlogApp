59f339ea6e91ce0a84afab269dfdf780f76d8c6a
Eriks Petersons <eriksters@gmail.com>
2021-09-22 23:13:27 +0200

    Redux tracking account sign in

--- App/App.js
+++ App/App.js
@@ -6,10 +6,6 @@ import {
   Baskervville_400Regular,
 } from "@expo-google-fonts/baskervville";
 import AppLoading from "expo-app-loading";
-import {
-  Provider as PaperProvider,
-  BottomNavigation,
-} from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import {
@@ -18,6 +14,10 @@ import {
   Ionicons,
   Octicons,
 } from "@expo/vector-icons";
+import { createStore, combineReducers } from "redux";
+import { Provider as ReduxProvider } from "react-redux";
+
+import { reducer as AccountReducer } from "./Redux/Reducers/AccountReducer";
 
 import CreateBlogPostScreen from "./Screens/CreateBlogPostScreen";
 import NewestBlogPostListTab from "./Screens/NewestBlogPostListTab";
@@ -30,122 +30,93 @@ export default function App() {
     Baskervville_400Regular,
   });
 
+  const preloadedState = {
+    account: {
+      signedIn: true,
+      accountId: 101010,
+      username: "Bob",
+      token: "TEST_JWT_TOKEN",
+    },
+  };
+
+  const reducers = combineReducers({ account: AccountReducer });
+  const reduxStore = createStore(reducers, preloadedState);
+
   const Tab = createBottomTabNavigator();
 
   if (fontsLoaded) {
     return (
-      <NavigationContainer>
-        <Tab.Navigator
-          initialRouteName={"Newest"}
-          screenOptions={{ headerShown: false }}
-        >
-          <Tab.Screen
-            name='Newest'
-            component={NewestBlogPostListTab}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons
-                  name='newspaper'
-                  size={size}
-                  color={color}
-                />
-              ),
-              tabBarLabel: "New",
-            }}
-          />
-          <Tab.Screen
-            name='Popular'
-            component={PopularBlogPostListTab}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Octicons name='flame' size={size} color={color} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name='Top'
-            component={TopBlogPostListTab}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name='ribbon-sharp' size={size} color={color} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name='Mine'
-            component={MyBlogPostListTab}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <MaterialIcons
-                  name='account-circle'
-                  size={size}
-                  color={color}
-                />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name='Remove Me!'
-            component={CreateBlogPostScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons
-                  name='plus-box'
-                  size={size}
-                  color={color}
-                />
-              ),
-            }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <ReduxProvider store={reduxStore}>
+        <NavigationContainer>
+          <Tab.Navigator
+            initialRouteName={"Newest"}
+            screenOptions={{ headerShown: false }}
+          >
+            <Tab.Screen
+              name='Newest'
+              component={NewestBlogPostListTab}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <MaterialCommunityIcons
+                    name='newspaper'
+                    size={size}
+                    color={color}
+                  />
+                ),
+                tabBarLabel: "New",
+              }}
+            />
+            <Tab.Screen
+              name='Popular'
+              component={PopularBlogPostListTab}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Octicons name='flame' size={size} color={color} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name='Top'
+              component={TopBlogPostListTab}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons name='ribbon-sharp' size={size} color={color} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name='Mine'
+              component={MyBlogPostListTab}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <MaterialIcons
+                    name='account-circle'
+                    size={size}
+                    color={color}
+                  />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name='Remove Me!'
+              component={CreateBlogPostScreen}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <MaterialCommunityIcons
+                    name='plus-box'
+                    size={size}
+                    color={color}
+                  />
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </ReduxProvider>
     );
   } else {
     return <AppLoading />;
   }
-
-  // const CreateBlogPostRoute = () => <NewBlogPostScreen />;
-  // const BlogPostListRoute = () => <BlogPostList />;
-
-  // const [index, setIndex] = useState(0);
-  // const [routes] = useState([
-  //   { key: "blogPostList", title: "List", icon: "view-list-outline" },
-  //   { key: "createBlogPost", title: "Create", icon: "plus-box" },
-  // ]);
-
-  // const renderScene = BottomNavigation.SceneMap({
-  //   blogPostList: BlogPostListRoute,
-  //   createBlogPost: CreateBlogPostRoute,
-  // });
-
-  // if (!fontsLoaded) {
-  //   return <AppLoading />;
-  // } else {
-  //   return (
-  //     <PaperProvider>
-  //       <StatusBar style={styles.statusBar} translucent={true} />
-  //       {/* <View style={styles.container}> */}
-  //       <BottomNavigation
-  //         navigationState={{ index, routes }}
-  //         onIndexChange={setIndex}
-  //         renderScene={renderScene}
-  //       />
-
-  //       {/* </View> */}
-  //     </PaperProvider>
-  //   );
-
-  // <PaperProvider>
-  //   <StatusBar style='auto' />
-  //   <NewBlogPostScreen />
-
-  //   <View style={styles.container}>
-  //     <BlogPostList />
-  //     <StatusBar style='auto' />
-  //   </View>
-  // </PaperProvider>
-  // );
-  // }
 }
 
 const styles = StyleSheet.create({
--- App/Screens/MyBlogPostListTab.js
+++ App/Screens/MyBlogPostListTab.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
+import { useSelector, useDispatch } from "react-redux";
+import { signIn, signOut } from "../Redux/Actions/AccountActions";
 
 const MyBlogPostListTab = () => {
+  const account = useSelector((state) => state.account);
+  const dispatch = useDispatch();
+
   return (
     <View style={styles.container}>
       <Text>My posts</Text>
+      <Text>
+        {account.signedIn ? "You are signed in" : "You should sign in"}
+      </Text>
+      <Button onPress={() => dispatch(signIn("Bob@snailmail.com", "Bob123"))}>
+        Log In
+      </Button>
+      <Button onPress={() => dispatch(signOut())}>Log Out</Button>
     </View>
   );
 };
